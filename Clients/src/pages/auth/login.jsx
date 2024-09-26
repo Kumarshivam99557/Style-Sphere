35988ae79a1d4f6e9747197efeabf5c3ae98772b
kumar <kumarshivam9006445358@gmail.com>
2024-09-26 19:15:47 +0530

    commest set

--- Clients/src/pages/auth/login.jsx
+++ Clients/src/pages/auth/login.jsx
@@ -14,7 +14,7 @@ const AuthLogin = () => {
     <div className="max-auto w-full max-w-md space-y-6">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Sign in your account make it
+          Sign in your account 
         </h1>
         <p className="mt-2">
           Don't have an account
