dee7f4cc0f7a333c13987970628585ed1f8ce1b3
kumar <kumarshivam9006445358@gmail.com>
2024-09-26 19:21:26 +0530

    user schema

---
+++ Server/Models/login.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+
+const loginSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+});
